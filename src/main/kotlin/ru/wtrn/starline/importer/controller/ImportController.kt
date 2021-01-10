d824a5da608a23734be487da4ea37868ddbb9140
Dmitry Romanov <me@wtrn.ru>
2021-01-10 13:34:28 +0300

    WIP: ImportController

--- src/main/kotlin/ru/wtrn/starline/importer/controller/ImportController.kt
+++ src/main/kotlin/ru/wtrn/starline/importer/controller/ImportController.kt
@@ -3,14 +3,16 @@ package ru.wtrn.starline.importer.controller
 import mu.KotlinLogging
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.wtrn.starline.importer.dto.RouteImportRequest
 
-@RestController("/api/v1/import")
+@RestController
+@RequestMapping("/api/v1/import")
 class ImportController {
     private val logger = KotlinLogging.logger {  }
 
-    @PostMapping("/route")
+    @PostMapping("route")
     fun importRoute(@RequestBody request: RouteImportRequest) {
         logger.info { "Handling request: $request" }
     }
--- src/main/kotlin/ru/wtrn/starline/importer/dto/RouteImportRequest.kt
+++ src/main/kotlin/ru/wtrn/starline/importer/dto/RouteImportRequest.kt
@@ -1,9 +1,10 @@
 package ru.wtrn.starline.importer.dto
 
 import java.time.Instant
+import java.time.LocalDateTime
 
 data class RouteImportRequest(
     val deviceId: String? = null,
-    val since: Instant,
-    val until: Instant
+    val since: LocalDateTime,
+    val until: LocalDateTime
 )
