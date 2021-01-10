7f1231db2755ba7217e2d591ddde3276d92ddc94
Dmitry Romanov <me@wtrn.ru>
2021-01-10 13:50:45 +0300

    WIP: StarlineClient interface

---
+++ src/main/kotlin/ru/wtrn/starline/importer/client/StarlineClient.kt
@@ -0,0 +1,44 @@
+package ru.wtrn.starline.importer.client
+
+import java.time.Duration
+import java.time.Instant
+
+interface StarlineClient {
+    fun getDevices(): List<StarlineDevice>
+    fun getRoute(deviceId: String, since: Instant, until: Instant): StarlineRoute
+}
+
+data class StarlineDevice(
+    val alias: String,
+    val deviceId: String
+)
+
+data class StarlineRoute(
+    val meta: RouteMeta,
+    val data: List<RouteDataNode>
+)
+
+data class RouteMeta(
+    val mileage: Long,
+    val movingTime: Duration,
+    val waitingTime: Duration
+)
+
+data class RouteDataNode(
+    val x: Float,
+    val y: Float,
+    val timestamp: Instant,
+    val satQty: Int,
+    val movingTime: Duration,
+    val waitingTime: Duration,
+    val mileage: Long,
+    val nodes: List<RouteTrackNode>
+)
+
+data class RouteTrackNode(
+    val x: Float,
+    val y: Float,
+    val timestamp: Instant,
+    val satQty: Int,
+    val mileage: Long
+)
--- src/main/kotlin/ru/wtrn/starline/importer/dto/RouteImportRequest.kt
+++ src/main/kotlin/ru/wtrn/starline/importer/dto/RouteImportRequest.kt
@@ -1,10 +1,10 @@
 package ru.wtrn.starline.importer.dto
 
 import java.time.Instant
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 data class RouteImportRequest(
     val deviceId: String? = null,
-    val since: LocalDateTime,
-    val until: LocalDateTime
+    val since: ZonedDateTime,
+    val until: ZonedDateTime
 )
