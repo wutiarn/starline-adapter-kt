86573ce7d5c3184c059a6225d1c63a4d9ad43f31
Dmitry Romanov <me@wtrn.ru>
2021-01-10 16:51:06 +0300

    StarlineAuthHolder: Implement login

--- src/main/kotlin/ru/wtrn/starline/importer/client/StarlineAuthHolder.kt
+++ src/main/kotlin/ru/wtrn/starline/importer/client/StarlineAuthHolder.kt
@@ -2,24 +2,40 @@ package ru.wtrn.starline.importer.client
 
 import mu.KotlinLogging
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.client.support.HttpRequestWrapper
 import ru.wtrn.starline.importer.client.dto.StarlineLoginRequest
 import ru.wtrn.starline.importer.client.exception.AuthenticationFailedException
 import ru.wtrn.starline.importer.configuration.properties.StarlineApiProperties
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class StarlineAuthHolder(private val apiProperties: StarlineApiProperties) {
     private val httpClient = RestTemplateBuilder().rootUri(apiProperties.baseUrl).build()
     private val logger = KotlinLogging.logger { }
 
+    private var cookieHeader: String? = null
+    private val cookieHeaderLock = ReentrantLock()
+
     fun addAuthenticationToRequest(httpRequest: HttpRequest): HttpRequest {
         val wrapper = HttpRequestWrapper(httpRequest)
-
+        wrapper.headers.set(HttpHeaders.COOKIE, getCookieHeader())
         return wrapper
     }
 
     fun refreshAuthentication() {
+        doRefreshAuthentication()
+    }
+
+    private fun getCookieHeader(): String {
+        cookieHeaderLock.withLock {
+            return cookieHeader ?: doRefreshAuthentication()
+        }
+    }
+
+    private fun doRefreshAuthentication(): String {
         val request = StarlineLoginRequest(
             username = apiProperties.username,
             password = apiProperties.password
@@ -29,11 +45,11 @@ class StarlineAuthHolder(private val apiProperties: StarlineApiProperties) {
             throw AuthenticationFailedException(response.body)
         }
 
-        val cookies = response.headers["Set-Cookie"]
+        val cookies = response.headers[HttpHeaders.SET_COOKIE]
             ?: throw IllegalStateException("Successful login request did not return Set-Cookie headers")
         val cookieHeader = composeCookieHeader(cookies)
-
         logger.info { "Starline authentication refreshed" }
+        return cookieHeader
     }
 
     private fun composeCookieHeader(setCookieValues: List<String>): String {
