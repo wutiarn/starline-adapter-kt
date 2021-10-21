1e74dc3aa1054afe161bb7e70096292186eb3355
Dmitry Romanov <me@wtrn.ru>
2021-10-21 08:13:21 +0300

    WIP: setup protobuf

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
 	id("org.springframework.boot") version "2.5.5"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	id("com.google.protobuf") version "0.8.13"
 	kotlin("jvm") version "1.5.31"
 	kotlin("kapt") version "1.5.31"
 	kotlin("plugin.spring") version "1.5.31"
@@ -28,8 +30,39 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.github.microutils:kotlin-logging-jvm:2.0.10")
+
+	implementation("io.grpc:grpc-kotlin-stub:1.2.0")
+	implementation("io.grpc:grpc-protobuf:1.41.0")
+	implementation("com.google.protobuf:protobuf-kotlin:3.19.0")
+
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.19.0"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java:1.2.0"
+		}
+		id("grpckt") {
+			artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.0:jdk7@jar"
+		}
+	}
+	generateProtoTasks {
+		all().forEach {
+			it.plugins {
+				id("grpc")
+				id("grpckt")
+			}
+			it.builtins {
+				id("kotlin")
+			}
+		}
+	}
 }
 
 tasks.withType<KotlinCompile> {
